8f5de4dd25dfa64a18b7c3a67e9df1599e244e9e
hyuniverse <hyuniverse12@gmail.com>
2025-02-15 22:08:30 +0900

    feat:[WIP]KqueueDemultiplexer 생성자 수정(#5)

--- src/demultiplexer/KqueueDemultiplexer.cpp
+++ src/demultiplexer/KqueueDemultiplexer.cpp
@@ -29,6 +29,7 @@ void	KqueueDemultiplexer::addSocketImpl(int fd) {
 	EV_SET(&changes[1], fd, EVFILT_EXCEPT, EV_ADD, 0, 0, nullptr);
 	changedEvents_.insert(changedEvents_.end(), changes, changes + 2);
 }
+
 void	KqueueDemultiplexer::removeSocketImpl(int fd) {
 	struct kevent changes[3];
 
@@ -37,12 +38,14 @@ void	KqueueDemultiplexer::removeSocketImpl(int fd) {
 	EV_SET(&changes[2], fd, EVFILT_WRITE, EV_DELETE, 0, 0, nullptr);
 	changedEvents_.insert(changedEvents_.end(), changes, changes + 3);
 }
+
 void	KqueueDemultiplexer::addWriteEventImpl(int fd) {
 	struct kevent change;
 
 	EV_SET(&change, fd, EVFILT_WRITE, EV_ADD, 0, 0, nullptr);
 	changedEvents_.push_back(change);
 }
+
 void	KqueueDemultiplexer::removeWriteEventImpl(int fd) {
 	struct kevent change;
 	

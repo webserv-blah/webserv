50086fc572fcd3c65773fa2f6087eb873a00138f
hyuniverse <hyuniverse12@gmail.com>
2025-02-15 21:00:28 +0900

    feat: KqueueDemultiplexer 및 Demultiplexer 구현(#5)

--- src/demultiplexer/DemultiplexerBase.hpp
+++ src/demultiplexer/DemultiplexerBase.hpp
@@ -1,33 +1,30 @@
 #pragma once
-#define MAX_EVENT 100
+
+#define MAX_EVENT 1024
+#define READ_EVENT 1
+#define WRITE_EVENT 2
+#define EXCEPTION_EVENT 3
+#define UNKNOWN_EVENT -1
+//enum으로 수정 예정.
+/*
+enum class EventType {
+    READ = 1,
+    WRITE = 2,
+    EXCEPTION = 3,
+    UNKNOWN = -1
+};
+*/
 
 template<typename Derived>
 class DemultiplexerBase {
 	public:
-		int waitForEvent() {
-			return static_cast<Derived*>(this)->waitForEventImpl();
-		}
-		void addSocket() {
-			return static_cast<Derived*>(this)->addSocketImpl();
-		}
-		void removeSocket() {
-			return static_cast<Derived*>(this)->removeSocketImpl();
-		}
-		void addWriteEvent() {
-			return static_cast<Derived*>(this)->addWriteEventImpl();
-		}
-		void removeWriteEvent() {
-			return static_cast<Derived*>(this)->removeWriteEventImpl();
-		}
-		bool isReadEvent() {
-			return static_cast<Derived*>(this)->isReadEventImpl();
-		}
-		bool isWriteEvent() {
-			return static_cast<Derived*>(this)->isWriteEventImpl();
-		}
-		bool isExceptionEvent() {
-			return static_cast<Derived*>(this)->isExceptionEventImpl();
-		}
+		int		waitForEvent(); 
+		void	addSocket(int fd);
+		void	removeSocket(int fd);
+		void	addWriteEvent(int fd);
+		void	removeWriteEvent(int fd);
+		int		getEventType(int idx);
+		int		getSocketFd(int idx);
 	
 	protected:
 		DemultiplexerBase() = default;

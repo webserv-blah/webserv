52c0d803738dfb6455519da29077d2ef3faaf835
hyuniverse <hyuniverse12@gmail.com>
2025-02-16 19:39:39 +0900

    feat: TimeoutHandler.updateActivity() 구현.(#3)

--- src/timeoutHandler/TimeoutHandler.cpp
+++ src/timeoutHandler/TimeoutHandler.cpp
@@ -1,6 +1,5 @@
 #include "TimeoutHandler.hpp"
 
-
 TimeoutHandler::TimeoutHandler() {
 
 }
@@ -9,6 +8,7 @@ TimeoutHandler::~TimeoutHandler() {
 
 }
 
+//TimeoutHandler 객체에서 관리할 연결시간 정보 추가
 void    TimeoutHandler::addConnection(int fd) {
     time_t lastActivity = getTime();
     connections_[fd] = lastActivity;
@@ -18,10 +18,10 @@ void    TimeoutHandler::addConnection(int fd) {
     expireMap_[fd] = it;
 }
 
+//최근 활동 시각 및 만료 시각 업데이트
 void    TimeoutHandler::updateActivity(int fd) {
     ConnectionIter cit = connections_.find(fd);
-    if (cit == connections_.end()) {
-        // connection not found
+    if (cit == connections_.end()) { // connection not found
         return ;
     }
     
@@ -38,7 +38,8 @@ void    TimeoutHandler::updateActivity(int fd) {
     expireMap_[fd] = qit;
 }
 
-void    TimeoutHandler::checkTimeOuts() {
+//만료된 연결 처리
+void    TimeoutHandler::checkTimeouts(EventHandler& eventHandler, Demultiplexer& reactor, ClientManager& clientManager) {
     time_t currentTime = getTime();
 
     while (!expireQueue_.empty()) {
@@ -48,21 +49,20 @@ void    TimeoutHandler::checkTimeOuts() {
             return ;
         }
 
-        // 만료 처리
-        //Q. eventHandler 어떻게 들고 있을 건지. 
-        //+ 그 외 clientSocket, demultiplexer에서의 제거 처리
         int fd = it->second;
-        {
-            //send 408 error
-            // eventHandler.handleError(408);
-            
-            // clean up clientSocket info
+        
+		//send 408 error
+		//실제 handleError 구현 후 인자 수정 예정
+		eventHandler.handleError(408); 
 
-        }
+		//client 정보 삭제
         removeConnection(fd, it);
+		reactor.removeSocket(fd);
+		clientManager.removeClient(fd); // Q. 내부에서 close(fd)할건지
     }
 }
 
+//연결 정보 제거(by fd) - 주로 객체 외부에서 사용
 void TimeoutHandler::removeConnection(int fd) {
     ExpireQueueIter it = expireMap_[fd];
 
@@ -71,13 +71,14 @@ void TimeoutHandler::removeConnection(int fd) {
     expireQueue_.erase(it);
 }
 
+//연결 정보 제거(by fd & iterator) - 객체 내부(checkTimeouts)에서 사용
 void TimeoutHandler::removeConnection(int fd, ExpireQueueIter it) {
     connections_.erase(fd);
     expireMap_.erase(fd);
     expireQueue_.erase(it);
 }
 
+//현재 시각(초) 반환
 time_t  TimeoutHandler::getTime() const {
-    return time(NULL); //sec.
+    return time(NULL);
 }
-

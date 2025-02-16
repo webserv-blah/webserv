63d42beafd15eef3ff764ee4a2d276aff4d41277
hyuniverse <hyuniverse12@gmail.com>
2025-02-16 19:19:39 +0900

    Revert "feat: Merge branch 'main' into feat/timeoutHandler"
    This reverts commit f943d390641e85fa49560ea72be9c45112c99d2a, reversing
    changes made to 289ad73208ee14cdbcbd46aa37ceedac8bcd11d1.

---
+++ src/clientManager/clientManager.cpp
@@ -0,0 +1,12 @@
+#include "ClientManager.hpp"
+
+ClientManager(int fd) : clientFd_(fd) {}
+~ClientManager() {}
+
+std::string ClientManager::readRequest() {
+	//@taerakim
+}
+
+/*type*/ ClientManager::writeResponse() {
+	// @damin
+}

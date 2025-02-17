9baecc676e4ead5003c685eb8d9d6fa9ec88fa6a
taerankim <taetaeee02@gmail.com>
2025-02-17 19:57:33 +0900

    feat: RequestParser 구현, 유효한 요청메시지 성공

---
+++ src/RequestParser/RequestParser.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "RequestMessage.hpp"
+
+#define ONELINE_MAX_LENGTH	8190//8KB - 2bytes(\r\n)
+#define URI_MAX_LENGTH		2048//2KB
+
+class RequestParser {
+	public:
+		RequestParser();
+		~RequestParser();
+
+		std::string			parse(const std::string &readData, RequestMessage &reqMsg);
+		//void				setBodyMaxLength(ssize_t length);
+
+	private:
+		ssize_t				oneLineMaxLength_;
+		ssize_t				uriMaxLength_;
+		//Optional<ssize_t>	bodyMaxLength_;//Client마다 바뀌는 설정 값
+		
+		enum requestStatus	setStatusCRLF(const enum requestStatus &curStatus);
+		void				parseStartLine(const std::string &line, RequestMessage &reqMsg);
+		void				parseFieldLine(const std::string &line, RequestMessage &reqMsg);
+		void				parseBody(const std::string &line, RequestMessage &reqMsg);
+		//void				cleanUpChunkedBody(const std::string &line);
+};
+

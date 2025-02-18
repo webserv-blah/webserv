f8f5ecf2ddb38e1593da803db87ac7929ea0dd2b
taerankim <taetaeee02@gmail.com>
2025-02-18 14:39:34 +0900

    feat: RequestMessage 메타 데이터 추가 및 Status 클린업

--- src/RequestMessage/RequestMessage.hpp
+++ src/RequestMessage/RequestMessage.hpp
@@ -4,30 +4,23 @@
 #include <map>
 #include <vector>
 
-#define CRLF "\r\n"
-enum Method { INIT, GET, POST, DELETE };
-////////////////buffersize를 굉장히 작게도 해봐야겠다
-//해당 요소까지 완성되었음을 알림
-enum requestStatus {
-	REQ_INIT,				// 생성됨
-	REQ_TOP_CRLF,			// CRLF 1개 허용됨
-	REQ_METHOD,				// 완료 => 완료이전에 판단중에 iss.peek() == EOF라면 기다려봐야함
-	REQ_TARGET_URI,			// 완료
-	REQ_STARTLINE,			// start-line 완료
-	REQ_STARTLINE_CRLF,		// start-line 이후 종료 상태 -> DONE으로 통합할지 고민
-	REQ_HEADER_FIELD_ING,	// 
-	REQ_HEADER_FIELD,		// 
-	REQ_HEADER_CRLF,		// 
-	REQ_BODY_ING,			// 
-	REQ_BODY,				// 
-	REQ_DONE,				// 
-	REQ_ERROR				// 
-};
+typedef enum Method { INIT, GET, POST, DELETE } TypeMethod;
+typedef enum connection { KEEP_ALIVE, CLOSE } TypeConnection;
+typedef enum requestStatus {
+	REQ_INIT,
+	REQ_TOP_CRLF,		// CRLF(0,1)
+	REQ_METHOD,			// 완료(1) => 완료이전에 판단중에 iss.peek() == EOF라면 기다려봐야함
+	REQ_TARGET_URI,		// 완료(1)
+	REQ_STARTLINE,		// start-line 완료
+	REQ_HEADER_FIELD,	// 진행중(N)
+	REQ_HEADER_CRLF,	// header-field 완료
+	REQ_BODY,			// 진행중(N)
+	REQ_DONE,
+	REQ_ERROR
+} TypeReqStatus;
 
 class RequestMessage {
 	public:
-		typedef enum Method 										TypeMethod;
-		typedef enum requestStatus									TypeReqStatus;
 		typedef std::map<std::string, std::vector<std::string> >	TypeField;
 
 		RequestMessage();
@@ -47,29 +40,31 @@ class RequestMessage {
 		
 		TypeReqStatus	getStatus() const;
 		std:: string	getMetaHost() const;
-		ssize_t			getMetaConnection() const;
+		TypeConnection	getMetaConnection() const;
 		ssize_t			getMetaContentLength() const;
 		void			setStatus(TypeReqStatus status);
 		void			setMetaHost(std::string value);
-		void			setMetaConnection(ssize_t value);
-		void			setMetaContentLength(ssize_t value);
+		void			setMetaConnection(std::string value);
+		void			setMetaContentLength(std::string value);
 
-		void printResult() const;//////////////////////////
-		void printFields() const;//////////////////////////
-		void printBody() const;//////////////////////////
+		// 파싱 후, 결과 출력을 위한 함수
+		void printResult() const;
+		void printFields() const;
+		void printBody() const;
+		void printMetaData(void) const;
+		
+		// exception class 만드는게 유용할지도..
+
 	private:
-		//0,1 -> 1개까지 허용
 		TypeMethod		method_;
 		std::string		targetURI_;
-		//O -> 종료
 		TypeField		fieldLines_;
-		//O -> 종료 or body
 		std::string		body_;
 
 		//meta data
 		TypeReqStatus	status_;
 		std::string		metaHost_;
-		ssize_t			metaConnection_;
+		TypeConnection	metaConnection_;
 		ssize_t			metaContentLength_;
 		//...2
 };

06d1b0d4990e03d9cc20858b632db2f14ebb5be0
Daewon Min <damin@c10r3s13.42seoul.kr>
2025-02-18 16:44:32 +0900

    style: Enum 컨벤션 반영

--- src/RequestHandler/file_utils.hpp
+++ src/RequestHandler/file_utils.hpp
@@ -7,7 +7,7 @@
 #include <vector>
 
 namespace fileUtilities {
-	enum ValidationResult {
+	enum EnumValidationResult {
 		VALID_PATH,         // 유효한 경로 (디렉터리) 이며 권한 OK
 		PATH_NOT_FOUND,     // 디렉터리로 볼 수 있는 경로가 없음
 		PATH_NO_PERMISSION, // 디렉터리가 존재하지만 접근 권한 없음
@@ -19,6 +19,6 @@ namespace fileUtilities {
 	};
 
 	//  존재 / 권한 상태를 7가지 결과 중 하나로 반환
-	ValidationResult	validatePath(const std::string& path);
-	std::string			readFile(const std::string &filePath);
+	EnumValidationResult	validatePath(const std::string& path);
+	std::string			    readFile(const std::string &filePath);
 };

ad90b06b078197474b70b77512e5a32773c7e68b
hyuniverse <hyuniverse12@gmail.com>
2025-02-18 14:20:32 +0900

    feat:Demltiplexer getEventType 반환형 수정(int->enum)(#32)

--- src/Demultiplexer/KqueueDemultiplexer.hpp
+++ src/Demultiplexer/KqueueDemultiplexer.hpp
@@ -12,14 +12,13 @@ class KqueueDemultiplexer : public DemultiplexerBase<KqueueDemultiplexer> {
 	public:
 		KqueueDemultiplexer(std::set<int>& serverFds);
 		~KqueueDemultiplexer();
-		int		waitForEventImpl();
-		void	addSocketImpl(int fd);
-		void	removeSocketImpl(int fd);
-		void	addWriteEventImpl(int fd);
-		void	removeWriteEventImpl(int fd);
-		
-		int		getEventTypeImpl(int idx);
-		int		getSocketFdImpl(int idx);
+		int			waitForEventImpl();
+		void		addSocketImpl(int fd);
+		void		removeSocketImpl(int fd);
+		void		addWriteEventImpl(int fd);
+		void		removeWriteEventImpl(int fd);
+		int			getSocketFdImpl(int idx);
+		TypeEvent	getEventTypeImpl(int idx);
 	
 	
 	private:

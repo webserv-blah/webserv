eebd2c699c4cf49b1032164b636fdf877e7fb6e3
taerankim <taetaeee02@gmail.com>
2025-02-19 03:01:24 +0900

    feat: ClientSession 객체 구현

---
+++ src/ClientSession/ClientSession.hpp
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "RequestParser.hpp"
+#include "GlobalConfig.hpp"
+#include "commonEnums.hpp"
+
+class ClientSession {
+	public:
+		ClientSession(int listenFd, int clientFd);
+		~ClientSession();
+
+		int					getListenFd() const;
+		int					getClientFd() const;
+		EnumSesStatus		getStatus() const;
+		std::string			getReadBuffer() const;
+		std::string			getWriteBuffer() const;
+		void				setListenFd(const int &listenFd);
+		void				setClientFd(const int &clientFd);
+		void				setStatus(const EnumSesStatus &status);
+		void				setReadBuffer(const std::string &remainData);
+		void				setWriteBuffer(const std::string &remainData);
+		
+		//Optional<ssize_t>	getConfBodyMax() const;
+		EnumSesStatus		implementReqMsg(RequestParser &parser, const std::string &readData);
+
+	private:
+		int					listenFd_;
+		int					errorStatusCode_;
+		int					clientFd_;
+		EnumSesStatus		status_;
+		RequestMessage		*reqMsg_;
+		RequestConfig		*config_;
+		std::string			readBuffer_;
+		std::string			writeBuffer_;
+};

d24882e03a38a302d0464e06eefeeebc16a6a6cb
hyuniverse <hyuniverse12@gmail.com>
2025-02-18 14:13:13 +0900

    feat: ServerManger client 관리 방버 업데이트 + enum 적용(#32)

--- src/ServerManager/ServerManagerRun.cpp
+++ src/ServerManager/ServerManagerRun.cpp
@@ -1,10 +1,6 @@
 #include "ServerManager.hpp"
-#include "../demultiplexer/KqueueDemultiplexer.hpp"
 
-//Event Loop
-// 읽기 혹은 쓰기 상태 관련 enum(혹은 매크로) 논의 필요
-// 아래 사용된 status 매크로는 임시
-
+// EventLoop
 void ServerManager::run() {
 	Demultiplexer	reactor(serverFds_);
 	EventHandler 	eventHandler();
@@ -12,49 +8,67 @@ void ServerManager::run() {
 	ClientManager	clientManager();
 
 	while (isRunning()) { //g_signal 변수 명을 isRunning으로 둬도 될 듯
-		int numEvents = reactor.waitForEvent();
+		int	numEvents = reactor.waitForEvent();
 
 		for (int i = 0; i < numEvents; ++i) {
-			int type = reactor.getEventType(i);
-			int fd = reactor.getSocketFd(i);
+			TypeEvent	type = reactor.getEventType(i);
+			int 		fd = reactor.getSocketFd(i);
 
-			if (type == EXCEPTION_EVENT) {
+			if (type == TypeEvent::EXCEPTION_EVENT) {
 				// error response 전송 여부 판단 후 추가
-				timeoutHandler.removeConnection(fd);
-				reactor.removeSocket(fd);
-			} else if (type == READ_EVENT) {
+				removeClientInfo(fd, clientManager, reactor, timeoutHandler);
+			} else if (type == TypeEvent::READ_EVENT) {
 				if (isServer(fd)) {
-					int clientFd = eventHandler.handleServerReadEvent(clientManager);
+					int clientFd = eventHandler.handleServerReadEvent(fd);
+
 					if (clientFd) {
-						reactor.addSocket(fd);
-						timeoutHandler.addConnection(clientFd);
+						addClientInfo(clientFd, clientManager, reactor, timeoutHandler);
 					}
+
 				} else {
-					int status = eventHandler.handleClientReadEvent();
-					if (status == CONNECTION_CLOSED) {
-						timeoutHandler.removeConnection(fd);
-						reactor.removeSocket(fd);
-					} else if (status == WRITE_ONGOING) {
+					TypeSesStatus	status = eventHandler.handleClientReadEvent(clientManager.accessClientSession(fd));
+
+					if (status == TypeSesStatus::CONNECTION_CLOSED) {
+						removeClientInfo(fd, clientManager, reactor, timeoutHandler);
+					} else if (status == TypeSesStatus::CONNECTION_ERROR) {
+						// connection error 처리 로직 추가
+					} else if (status == TypeSesStatus::WRITE_CONTINUE) {
 						timeoutHandler.updateActivity(fd);
 						reactor.addWriteEvent(fd);
 					} else {
 						timeoutHandler.updateActivity(fd);
 					}
 				}
-			} else if (type == WRITE_EVENT) 
-				int status = eventHandler.handleClientWriteEvent();
-				if (status == CONNECTION_CLOSED) {
-					timeoutHandler.removeConnection(fd);
-					reactor.removeSocket(fd);
-				} else if (status == DONE_WRITING) {
+
+			} else if (type == TypeEvent::WRITE_EVENT) {
+				TypeSesStatus	status = eventHandler.handleClientWriteEvent(clientManager.accessClientSession(fd));
+
+				if (status == TypeSesStatus::CONNECTION_CLOSED) {
+					removeClientInfo(fd, clientManager, reactor, timeoutHandler);
+				} else if (status == TypeSesStatus::WRITE_COMPLETE) {
 					timeoutHandler.updateActivity(fd); // 지울지 여부 판단 필요
 					reactor.removeWriteEvent(fd);
 				} else {
 					timeoutHandler.updateActivity(fd);
 				}
+
 			} 
 		}
 		timeoutHandler.checkTimeouts(eventHandler, reactor, clientManager);
 	}
 
 }
+
+// ServerManager의 멤버 함수로 둘지, 별도의 함수로 둘지
+void ServerManager::addClientInfo(int clientFd, ClientManager& clientManager, Demultiplexer& reactor, & timeoutHandler) {
+	clientManager.addClient(clientFd);
+	timeoutHandler.addConnection(clientFd);
+	reactor.addSocket(clientFd);
+}
+
+void ServerManager::removeClientInfo(int clientFd, ClientManager& clientManager, Demultiplexer& reactor, & timeoutHandler) {
+	clientManager.removeClient(clientFd);
+	timeoutHandler.removeConnection(clientFd);
+	reactor.removeSocket(clientFd);
+}
+

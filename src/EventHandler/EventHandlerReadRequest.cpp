521461f5d862a394118bd918fed8a24f203bfa56
taerankim <taetaeee02@gmail.com>
2025-02-21 15:27:55 +0900

    feat: readRequest 함수 추가

--- src/EventHandler/EventHandlerReadRequest.cpp
+++ src/EventHandler/EventHandlerReadRequest.cpp
@@ -1,6 +1,17 @@
 #include "EventHandler.hpp"
 
 int	EventHandler::readRequest() {
+	std::string buffer;
+	buffer.resize(BUFFER_SIZE);
 
+	ssize_t res = recv(curSession.getClientFd(), &buffer[0], BUFFER_SIZE, MSG_DONTWAIT);
+	if (res == -1) {
+		std::cerr << "Error: systemcall error" << std::endl;
+		return (CONNECTION_ERROR);
+	} else if (res > 0) {
+		EnumSesStatus requestResult;
+		requestResult = curSession.implementReqMsg(this->parser_, buffer.substr(res));
+		return requestResult;
+	}
 }
 

8e7a9621ca3673427ab426430578bf5874bd729b
hyuniverse <hyuniverse12@gmail.com>
2025-02-21 15:32:50 +0900

    refactor: ClientManager 수정사항 반영(#73)

--- src/ClientManager/ClientManager.hpp
+++ src/ClientManager/ClientManager.hpp
@@ -1,26 +1,23 @@
-#pragma once
+#ifndef CLIENT_MANAGER_HPP
+#define CLIENT_MANAGER_HPP
+
+#include "ClientSession.hpp"
 #include <map>
 #include <unistd.h>
 
-//임시
-class ClientSession {
-	public:
-		explicit ClientSession(int fd);
-		~ClientSession();
-		
-};
-
 class ClientManager {
 	public:
-		typedef std::map<int, ClientSession*>	TypeClientSessionMap;
+		typedef std::map<int, ClientSession*>	TypeClientMap;
 		ClientManager();
 		~ClientManager();
-		void						addClient(int fd);
-		ClientSessionMap::iterator	removeClient(int fd);
-		ClientSession				accessClientSession(int fd);
-		ClientSessionMap			accessClientSessionMap();
+		void					addClient(int listenFd, int clientFd);
+		TypeClientMap::iterator	removeClient(int fd);
+		ClientSession*			accessClientSession(int fd);
+		TypeClientMap&			accessClientSessionMap();
 
 	private:
-		TypeClientSessionMap	clientList_;
+		TypeClientMap	clientList_;
 
 };
+
+#endif

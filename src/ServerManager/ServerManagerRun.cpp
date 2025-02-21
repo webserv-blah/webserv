f3e15616b910d57919ab466ca22ea784031c015f
hyuniverse <hyuniverse12@gmail.com>
2025-02-21 15:33:55 +0900

    refactor: ClientManager 메소드 변동 사항 적용(#73)

--- src/ServerManager/ServerManagerRun.cpp
+++ src/ServerManager/ServerManagerRun.cpp
@@ -21,12 +21,17 @@ void ServerManager::run() {
 					int clientFd = eventHandler.handleServerReadEvent(fd);
 
 					if (clientFd > 0) { // 새로운 클라이언트가 정상적으로 연결됨
-						addClientInfo(clientFd, clientManager, reactor, timeoutHandler);
+						addClientInfo(fd, clientFd, clientManager, reactor, timeoutHandler);
 					}
 
 				} else { // 클라이언트 소켓에서 데이터 수신 처리
-					EnumSesStatus	status = eventHandler.handleClientReadEvent(clientManager.accessClientSession(fd));
+					ClientSession*	client = clientManager.accessClientSession(fd);
+					if (!client) {
+						perror("Invalid Client Fd");
+						continue ;
+					}
 
+					EnumSesStatus	status = eventHandler.handleClientReadEvent(*client);
 					if (status == CONNECTION_CLOSED) { // 클라이언트가 연결 종료
 						removeClientInfo(fd, clientManager, reactor, timeoutHandler);
 					} else if (status == WRITE_CONTINUE) { // 추가적인 쓰기 작업 필요
@@ -38,8 +43,13 @@ void ServerManager::run() {
 				}
 
 			} else if (type == WRITE_EVENT) { // 쓰기(송신) 이벤트 발생 시
-				EnumSesStatus	status = eventHandler.handleClientWriteEvent(clientManager.accessClientSession(fd));
+				ClientSession*	client = clientManager.accessClientSession(fd);
+				if (!client) {
+					perror("Invalid Client Fd");
+					continue ;
+				}
 
+				EnumSesStatus	status = eventHandler.handleClientWriteEvent(*client);
 				if (status == CONNECTION_CLOSED) { // 클라이언트가 연결 종료
 					removeClientInfo(fd, clientManager, reactor, timeoutHandler);
 				} else if (status == WRITE_COMPLETE) { // 데이터 전송 완료
@@ -48,6 +58,7 @@ void ServerManager::run() {
 				} else { // 쓰기 작업 진행 중 -> 타임아웃 갱신
 					timeoutHandler.updateActivity(fd); // 타임아웃 갱신((지울지 여부 판단 필요))
 				}
+
 			}
 		}
 		// 타임아웃된 클라이언트 처리
@@ -60,7 +71,7 @@ void ServerManager::run() {
 
 // 서버 종료 전, 모든 클라이언트에 종료 응답을 전송하고 연결된 리소스를 정리하는 함수
 void ServerManager::cleanUpConnections(ClientManager& clientManager, EventHandler& eventHandler) {
-	std::map<int, ClientSession*>			clientList = clientManager.accessClientSessionMap();
+	std::map<int, ClientSession*>&			clientList = clientManager.accessClientSessionMap();
 	std::map<int, ClientSession*>::iterator	it;
 
 	for (it = clientList.begin(); it != clientList.end(); ) {
@@ -71,8 +82,8 @@ void ServerManager::cleanUpConnections(ClientManager& clientManager, EventHandle
 
 // ((ServerManager 멤버 함수로 둘지, 별도의 함수로 만들지 고민 필요))
 // 새로운 클라이언트 정보 추가 함수
-void ServerManager::addClientInfo(int clientFd, ClientManager& clientManager, Demultiplexer& reactor, TimeoutHandler& timeoutHandler) {
-	clientManager.addClient(clientFd); // 클라이언트 추가
+void ServerManager::addClientInfo(int listenFd, int clientFd, ClientManager& clientManager, Demultiplexer& reactor, TimeoutHandler& timeoutHandler) {
+	clientManager.addClient(listenFd, clientFd); // 클라이언트 추가
 	timeoutHandler.addConnection(clientFd); // 타임아웃 관리 추가
 	reactor.addSocket(clientFd); // 리액터(이벤트 루프)에 소켓 등록
 }

b4fe447c17b5123778884974c5eab4a6945c49e8
hyuniverse <hyuniverse12@gmail.com>
2025-02-23 20:41:13 +0900

    refactor: TimeoutHandler 리팩토링(#89)
    - 컨벤션 적용(typedef, 헤더 인클루드 등)
    - 주석 추가
    - 예외 처리 추가
    - 매크로를 static const int로 변경
    - perror를 cerr로 변경
    - TODO 1건 추가(error 전송 실패 시 처리)
    - removeConnection 중복 개선

--- src/TimeoutHandler/TimeoutHandler.hpp
+++ src/TimeoutHandler/TimeoutHandler.hpp
@@ -1,36 +1,38 @@
 #ifndef TIMEOUT_HANDLER_HPP
 #define TIMEOUT_HANDLER_HPP
 
+#include "EventHandler.hpp"
+#include "KqueueDemultiplexer.hpp"
+#include "ClientManager.hpp"
 #include <map>
 
-#include "../EventHandler/EventHandler.hpp"
-#include "../Demultiplexer/KqueueDemultiplexer.hpp"
-#include "../ClientManager/ClientManager.hpp"
-
-#define LIMIT 60 //sec => 만료 시간 논의
+// Nginx의 client_body_timeout 값과 동일 (초 단위 제한 시간)
+static const int LIMIT = 60;
 
 class TimeoutHandler {
     public:
-        typedef std::map<int, time_t>::iterator         ConnectionIter;
-        typedef std::multimap<time_t, int>::iterator    ExpireQueueIter;
-        typedef std::map<int, ExpireQueueIter>          ExpireIterMap;
-        typedef ExpireIterMap::iterator                 ExpireIterMapIter;
+        typedef std::map<int, time_t>::iterator         TypeConnectionIter;
+        typedef std::multimap<time_t, int>::iterator    TypeExpireQueueIter;
+        typedef std::map<int, TypeExpireQueueIter>      TypeExpireIterMap;
+        typedef TypeExpireIterMap::iterator             TypeExpireIterMapIter;
         
         TimeoutHandler();
         ~TimeoutHandler();
-        
+
         void addConnection(int fd);
         void updateActivity(int fd);
         void checkTimeouts(EventHandler& eventHandler, Demultiplexer& reactor, ClientManager& clientManager);
         void removeConnection(int fd);
 
     private:
-        std::map<int, time_t>       connections_;
-        std::multimap<time_t, int>  expireQueue_;
-        ExpireIterMap               expireMap_;
-        
-        void removeConnection(int fd, ExpireQueueIter it);
+        std::map<int, time_t>       connections_; // 연결 정보 (fd -> 마지막 활동 시간)
+        std::multimap<time_t, int>  expireQueue_; // 만료 시간 기준으로 오름차순 정렬된 연결 목록
+        TypeExpireIterMap           expireMap_;   // fd -> expireQueue_의 iterator
+												  // expireQueue_에 fd값으로 빠르게 접근하기 위한 자료구조
+
+        void removeConnection(int fd, TypeExpireQueueIter it);
         time_t getTime() const;
+
 };
 
 #endif

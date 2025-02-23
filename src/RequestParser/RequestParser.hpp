636ff768086bc2369cef950ff535df8c04a058de
taerankim <taetaeee02@gmail.com>
2025-02-23 22:15:36 +0900

    feat: RequestParser 파싱함수 status code 리턴

--- src/RequestParser/RequestParser.hpp
+++ src/RequestParser/RequestParser.hpp
@@ -6,27 +6,29 @@
 #define ONELINE_MAX_LENGTH	8190//8KB - 2bytes(\r\n)
 #define URI_MAX_LENGTH		2048//2KB
 
+#define NONE_STATUS_CODE	0
+
 class RequestParser {
 	public:
 		RequestParser();
 		~RequestParser();
 
-		std::string		parse(const std::string &readData, RequestMessage &reqMsg);
-		//void			setBodyMaxLength(size_t length);
+		int					parse(const std::string &readData, std::string &readBuffer, RequestMessage &reqMsg);
+		//void				setBodyMaxLength(size_t length);
 
 	private:
-		size_t			oneLineMaxLength_;
-		size_t			uriMaxLength_;
+		size_t				oneLineMaxLength_;
+		size_t				uriMaxLength_;
 		//Optional<size_t>	bodyMaxLength_;//Client마다 바뀌는 설정 값
 		
-		void			handleOneLine(const std::string &line, RequestMessage &reqMsg);
-		EnumReqStatus	handleCRLFLine(const EnumReqStatus &curStatus);
-		void			parseStartLine(const std::string &line, RequestMessage &reqMsg);
-		void			parseFieldLine(const std::string &line, RequestMessage &reqMsg);
-		bool			validateFieldValueCount(const std::string &name, const int count);
-		bool			handleFieldValue(const std::string &name, const std::string &value, RequestMessage &reqMsg);
-		std::string		cleanUpChunkedBody(const std::string &data, RequestMessage &reqMsg);
-		std::string 	parseBody(const std::string &line, RequestMessage &reqMsg);
+		int					handleOneLine(const std::string &line, RequestMessage &reqMsg);
+		EnumReqStatus		handleCRLFLine(const EnumReqStatus &curStatus);
+		int					parseStartLine(const std::string &line, RequestMessage &reqMsg);
+		int					parseFieldLine(const std::string &line, RequestMessage &reqMsg);
+		bool				validateFieldValueCount(const std::string &name, const int count);
+		bool				handleFieldValue(const std::string &name, const std::string &value, RequestMessage &reqMsg);
+		int					cleanUpChunkedBody(const std::string &data, std::string &readBuffer, RequestMessage &reqMsg);
+		int					parseBody(const std::string &line, std::string &readBuffer, RequestMessage &reqMsg);
 };
 
 #endif

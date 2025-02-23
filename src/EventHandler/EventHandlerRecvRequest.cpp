245757a7e760bdb5ac778924cba712890ad377ad
taerankim <taetaeee02@gmail.com>
2025-02-23 23:30:27 +0900

    feat: recvRequest에서 recv함수의 반환값에 대한 처리 추가

--- src/EventHandler/EventHandlerRecvRequest.cpp
+++ src/EventHandler/EventHandlerRecvRequest.cpp
@@ -12,9 +12,15 @@ EnumSesStatus EventHandler::recvRequest(ClientSession &curSession) {
 
 	ssize_t res = recv(curSession.getClientFd(), &buffer[0], BUFFER_SIZE, MSG_DONTWAIT);
 	if (res == -1) {
-		std::cerr << "Error: systemcall error" << std::endl;
-		return (CONNECTION_ERROR);
-	} else if (res > 0) {
+		if (errno == EAGAIN
+		||  errno == EWOULDBLOCK)
+			return READ_DEFFERED;//다음 이벤트를 기다려야함
+		if (errno == EINTR)
+			return READ_CONTINUE;//일반적으로 재시도
+		return CONNECTION_CLOSED;//통신을 할 수 없는 상태이거나, 메모리 문제로 치명적인 경우
+	} else if (res == 0) {
+		return CONNECTION_CLOSED;//클라이언트 측에서 연결을 종료한 경우
+	} else {
 		EnumSesStatus requestResult;
 		requestResult = curSession.implementReqMsg(this->parser_, buffer.substr(res));
 		return requestResult;

36a5720165946f1df587ec8bb61115544f5cff66
taerankim <taetaeee02@gmail.com>
2025-02-24 14:55:35 +0900

    chore: recvRequest함수에 에러처리에 관한 주석 추가

--- src/EventHandler/EventHandlerRecvRequest.cpp
+++ src/EventHandler/EventHandlerRecvRequest.cpp
@@ -12,14 +12,14 @@ EnumSesStatus EventHandler::recvRequest(ClientSession &curSession) {
 
 	ssize_t res = recv(curSession.getClientFd(), &buffer[0], BUFFER_SIZE, MSG_DONTWAIT);
 	if (res == -1) {
-		if (errno == EAGAIN
-		||  errno == EWOULDBLOCK)
+		if (errno == EAGAIN || errno == EWOULDBLOCK)//non-blocking 모드에서 읽을 데이터가 없어 즉시 반환된 경우
 			return READ_DEFFERED;//다음 이벤트를 기다려야함
-		if (errno == EINTR)
+		if (errno == EINTR)//인터럽트, 시스템 호출 중 시그널에 의해 중단되어 호출이 완료되지 않은 경우
 			return READ_CONTINUE;//일반적으로 재시도
-		return CONNECTION_CLOSED;//통신을 할 수 없는 상태이거나, 메모리 문제로 치명적인 경우
-	} else if (res == 0) {
-		return CONNECTION_CLOSED;//클라이언트 측에서 연결을 종료한 경우
+		//통신을 할 수 없는 상태이거나, 메모리 문제로 치명적인 경우
+		return CONNECTION_CLOSED;
+	} else if (res == 0) {//클라이언트 측에서 연결을 종료한 경우
+		return CONNECTION_CLOSED;
 	} else {
 		EnumSesStatus requestResult;
 		requestResult = curSession.implementReqMsg(this->parser_, buffer.substr(res));

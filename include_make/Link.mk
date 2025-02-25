57eb1f991f39e13ffa7884ab7ae27a5276dd6d7e
taerankim <taetaeee02@gmail.com>
2025-02-25 20:21:02 +0900

    build: 명령어 실행문 출력 방지 처리

--- include_make/Link.mk
+++ include_make/Link.mk
@@ -1,8 +1,8 @@
 link_files::
 	@mkdir -p $(dst_dir);
-	$(foreach file,$(files), $(call color_printf,$(CYAN),$(file),🔗 linking file\n)ln -sf $(src_dir)/$(file) $(dst_dir);)
-	$(foreach file,$(files), ln -sf $(src_dir)/$(file) $(dst_dir)/$(file);)
+	$(Q)$(foreach file,$(files), $(call color_printf,$(CYAN),$(file),🔗 linking file\n)ln -sf $(src_dir)/$(file) $(dst_dir);)
+	$(Q)$(foreach file,$(files), ln -sf $(src_dir)/$(file) $(dst_dir)/$(file);)
 
 unlink_files::
-	$(foreach file,$(files), $(call color_printf,$(GRAY),$(file),🚫 unlinking file\n)$(RM) $(dst_dir)/$(file);)
-	$(foreach file,$(files), $(RM) $(dst_dir)/$(file);)
+	$(Q)$(foreach file,$(files), $(call color_printf,$(GRAY),$(file),🚫 unlinking file\n)$(RM) $(dst_dir)/$(file);)
+	$(Q)$(foreach file,$(files), $(RM) $(dst_dir)/$(file);)

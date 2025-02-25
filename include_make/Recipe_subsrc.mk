cef09152cdfc5b754d31b659d2c17cc5943f8657
taerankim <taetaeee02@gmail.com>
2025-02-25 20:16:50 +0900

    build: 빌드별 컬러타이틀 출력

--- include_make/Recipe_subsrc.mk
+++ include_make/Recipe_subsrc.mk
@@ -1,6 +1,10 @@
-all: $(NAME)
+all: 
+	$(call color_printf,$(YELLOW),$(NAME),🎯 starting compile $(NAME))
+	$(MAKE) $(NAME)
+	$(call color_printf,$(BLUE),$(NAME),🔰 done!)
 
 $(NAME): $(OBJS)
+	$(call color_printf,$(GREEN),$(NAME),📚 archive object)
 	$(AR) $(ARFLAGS) $@ $^
 	$(MAKE) files=$(NAME) src_dir=`pwd` dst_dir=$(SRCDIR) link_files
 	$(foreach head,$(HEAD), $(MAKE) files=$(head) src_dir=`pwd` dst_dir=$(SRCDIR)/include link_files;)
@@ -8,6 +12,7 @@ $(NAME): $(OBJS)
 clean:
 	$(MAKE) files=$(NAME) src_dir=`pwd` dst_dir=$(SRCDIR) unlink_files
 	$(foreach head,$(HEAD), $(MAKE) files=$(head) src_dir=`pwd` dst_dir=$(SRCDIR)/include unlink_files;)
+	$(call color_printf,$(RED),$(NAME),🗑️  remove Objects && Dependency file)
 	$(RM) $(OBJS) $(DEPS)
 	$(RM) $(NAME)
 

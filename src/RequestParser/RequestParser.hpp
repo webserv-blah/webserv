efbb401604766589c9a2ad5216614a0c5958784e
taerankim <taetaeee02@gmail.com>
2025-02-25 15:23:14 +0900

    chore: EnumStatusCode를 RequestParser와 ClientSession에 반영

--- src/RequestParser/RequestParser.hpp
+++ src/RequestParser/RequestParser.hpp
@@ -2,19 +2,18 @@
 #define REQUEST_PARSER_HPP
 
 #include "RequestMessage.hpp"
+#include "commonEnums.hpp"
 
 #define ONELINE_MAX_LENGTH	8190//8KB - 2bytes(\r\n)
 #define URI_MAX_LENGTH		2048//2KB
 #define BODY_MAX_LENGTH		1048576//1MB
-
-#define NONE_STATUS_CODE	0
 
 class RequestParser {
 	public:
 		RequestParser();
 		~RequestParser();
 
-		int					parse(const std::string &readData, std::string &readBuffer, RequestMessage &reqMsg);
+		EnumStatusCode		parse(const std::string &readData, std::string &readBuffer, RequestMessage &reqMsg);
 		void				setConfigBodyLength(size_t length);
 
 	private:
@@ -22,14 +21,14 @@ class RequestParser {
 		size_t				uriMaxLength_;
 		size_t				bodyMaxLength_;//Client마다 바뀌는 설정 값
 		
-		int					handleOneLine(const std::string &line, RequestMessage &reqMsg);
+		EnumStatusCode		handleOneLine(const std::string &line, RequestMessage &reqMsg);
 		EnumReqStatus		handleCRLFLine(const EnumReqStatus &curStatus);
-		int					parseStartLine(const std::string &line, RequestMessage &reqMsg);
-		int					parseFieldLine(const std::string &line, RequestMessage &reqMsg);
+		EnumStatusCode		parseStartLine(const std::string &line, RequestMessage &reqMsg);
+		EnumStatusCode		parseFieldLine(const std::string &line, RequestMessage &reqMsg);
 		bool				validateFieldValueCount(const std::string &name, const int count);
 		bool				handleFieldValue(const std::string &name, const std::string &value, RequestMessage &reqMsg);
-		int					cleanUpChunkedBody(const std::string &data, std::string &readBuffer, RequestMessage &reqMsg);
-		int					parseBody(const std::string &line, std::string &readBuffer, RequestMessage &reqMsg);
+		EnumStatusCode		cleanUpChunkedBody(const std::string &data, std::string &readBuffer, RequestMessage &reqMsg);
+		EnumStatusCode		parseBody(const std::string &line, std::string &readBuffer, RequestMessage &reqMsg);
 };
 
 #endif
